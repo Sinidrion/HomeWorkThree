467a8ac3f36606c419605a51894b48148eca0ea2
Sinidrion <52216001+Sinidrion@users.noreply.github.com>
2019-06-30 23:46:18 +0300

    Add files via upload

---
+++ Task6_34.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Task6_34 {
+
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 1;
+
+        for (int c = 100; b <= a; c++) {
+            if (c % 19 == 0) {
+                System.out.println(c);
+                b++;
+            }
+
+        }
+    }
+
+}
+
+
